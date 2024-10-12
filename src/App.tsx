cefd152152793d48ecfd2e3871b0ea2baee41ac7
VICTOR ISAI SANTANA <isai@raldoo.com>
2024-10-11 20:03:22 -0700

    se agregó la funcionalidad mutations 2, manejo de errores optimisitc, rollback de datos

--- src/App.tsx
+++ src/App.tsx
@@ -16,21 +16,42 @@ function App() {
   const queryClient = useQueryClient()
   const {mutate,isPending,error}= useMutation({
     mutationFn:(post:Post)=>
-      axios.post<Post>('https://jsonplaceholder.typicode.com/posts',post)
+      axios.post<Post>('https://jsonplaceholder.typicocde.com/posts',post)
       .then(response =>response.data),
-      //cuando el post tiene exito va retornar dos objetos, lo que se hace en la siguiente linea es:
-      //asignar esos elementos al key de posts para que puedan ser accesibles y se puedan ver en el listado
-    onSuccess:(savedPost,newPost)=>{
-      queryClient.setQueryData<Post[]>(['posts'],(post = [])=>[savedPost,...post])
+
+    onMutate:(newPost)=>{
+      const oldPost = queryClient.getQueryData<Post[]>(['posts'])
+
+      queryClient.setQueryData<Post[]>(['posts'],(post = [])=>[newPost,...post])
       
       if(titleRef.current?.value && bodyRef.current?.value){        
           titleRef.current.value = ''
           bodyRef.current.value = ''
       }
-
-
+      return oldPost
+    },
+      //cuando el post tiene exito va retornar dos objetos, lo que se hace en la siguiente linea es:
+      //asignar esos elementos al key de posts para que puedan ser accesibles y se puedan ver en el listado
+    onSuccess:(savedPost,newPost)=>{
+      //OPTIMISTIC UI
+      queryClient.setQueryData<Post[]>(
+        ['post'],
+        (posts = []) => posts.map((post)=> { 
+                              console.log(post,newPost)
+                              return post.id === newPost.id ? savedPost : post
+                              })
+      )
       // esto hace que se borre la cache
       // queryClient.invalidateQueries({queryKey:['posts']})
+    },
+    //actualizar estado de react query
+    onError:(error,newPost,ctx) => {
+      /*queryClient.setQueryData<Post[]>( ['posts'], (posts=[]) => {
+                                                                    return posts.filter(post=>post.id !== newPost.id)
+                                                          })*/
+
+      //CONTEXT                                                                    
+      queryClient.setQueryData<Post[]>(['posts'],ctx);
     }
   })
 
