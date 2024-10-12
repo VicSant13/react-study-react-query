0684ad611ad3243b3f10e57dcdfa2f181dc3417d
VICTOR ISAI SANTANA <isai@raldoo.com>
2024-10-11 19:41:06 -0700

    se agregó la funcionalidad mutations 2,envio de formulario y actualizacion del listado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import axios from 'axios'
+import { useRef } from "react"
 
 type Post = {
   id:number,
@@ -9,6 +10,30 @@ type Post = {
 }
 
 function App() {
+    //para podeder ejecutar el useMutation debes de mandar a llamar << mutate >>
+  const titleRef = useRef<HTMLInputElement>(null)
+  const bodyRef = useRef<HTMLInputElement>(null)
+  const queryClient = useQueryClient()
+  const {mutate,isPending}= useMutation({
+    mutationFn:(post:Post)=>
+      axios.post<Post>('https://jsonplaceholder.typicode.com/posts',post)
+      .then(response =>response.data),
+      //cuando el post tiene exito va retornar dos objetos, lo que se hace en la siguiente linea es:
+      //asignar esos elementos al key de posts para que puedan ser accesibles y se puedan ver en el listado
+    onSuccess:(savedPost,newPost)=>{
+      queryClient.setQueryData<Post[]>(['posts'],(post = [])=>[savedPost,...post])
+      
+      if(titleRef.current?.value && bodyRef.current?.value){        
+          titleRef.current.value = ''
+          bodyRef.current.value = ''
+      }
+
+
+      // esto hace que se borre la cache
+      // queryClient.invalidateQueries({queryKey:['posts']})
+    }
+  })
+
    const {data,isLoading}= useQuery({
       queryKey:['posts'],
       queryFn:()=>
@@ -19,12 +44,25 @@ function App() {
   return (
     <>
       <h2>Posts</h2>
-      <form>
+      <form onSubmit={e=>{
+        e.preventDefault()
+        if(titleRef.current?.value && bodyRef.current?.value){        
+        mutate({
+                id:0,
+                body:bodyRef.current.value,
+                title:titleRef.current.value,
+                userId:1
+        })
+      }
+      }}>
         <div>
-          <input type="text" />
+          <input ref={titleRef}type="text" placeholder="titulo"/>
         </div>
         <div>
-          <button>Enviar</button>
+          <input ref={bodyRef}type="text" placeholder="body"/>
+        </div>
+        <div>
+          <button disabled={isPending}>{isPending ? 'Creando':'Guardar'}</button>
         </div>
       </form>
       {isLoading && 'Cargando...'}
